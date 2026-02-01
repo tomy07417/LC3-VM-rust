0903cb375091894f975a51b80244c20a3200f31c
Tomy07417 <tamundarain@fi.uba.ar>
2026-02-01 17:52:21 +0300

    refactor: change the name of load function files

--- src/op/load/mod.rs
+++ src/op/load/mod.rs
@@ -1,7 +1,7 @@
-mod load_op;
-mod load_rg;
+mod load_ld;
+mod load_lrg;
 mod load_lea;
 
-pub use load_op::load;
-pub use load_rg::load_register;
+pub use load_ld::load;
+pub use load_lrg::load_register;
 pub use load_lea::load_effective_address;
